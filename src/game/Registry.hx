9dcebf39125f087cd4d144c78bd57f2e29050f14
Karlo <Karlo@localhost>
2013-11-22 00:38:58 +0800

    Can now setup the player class

--- src/game/Registry.hx
+++ src/game/Registry.hx
@@ -10,6 +10,8 @@ import game.managers.PlayerManager;
  */
 class Registry
 {
+	public static var colorList : Array<Int> = [ 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0x000033, 0xFFFFFF ];		
+	
 	static public var pack:AssetPack;
 	static public var playAreaPadding : Point = new Point(15, 15);
 	static public var inputManager:InputManager;
--- src/game/objects/Player.hx
+++ src/game/objects/Player.hx
@@ -7,6 +7,11 @@ import flambe.Component;
  */
 class Player extends Component
 {
+	public var territories:Array<Int>;
+	public var territoryColor:Int;
+	public var isHuman:Bool = false;
+	//public var ai:EnemyAI;	
+	public var playerNum:Int;
 
 	public function new() 
 	{
@@ -15,7 +20,14 @@ class Player extends Component
 	
 	public function initialize(playerNum : Int, isHuman : Bool )
 	{
+		this.playerNum = playerNum;
+		this.isHuman = isHuman;
 		
+		//if ( !this.isHuman )
+			//this.ai = new EnemyAI(this);
+		
+		this.territoryColor = Registry.colorList[playerNum-1];
+		this.territories = new Array<Int>();
 	}
 	
 }
